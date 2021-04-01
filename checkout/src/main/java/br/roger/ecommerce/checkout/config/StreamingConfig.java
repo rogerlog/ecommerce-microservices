dec7da636e0a41ca2ef3d2e203d7e5964978771a
Roger <logroger@gmail.com>
2021-04-01 20:43:25 -0300

    config files

---
+++ checkout/src/main/java/br/roger/ecommerce/checkout/config/StreamingConfig.java
@@ -0,0 +1,13 @@
+package br.roger.ecommerce.checkout.config;
+
+import br.roger.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+//        PaymentPaidSink.class
+})
+public class StreamingConfig {
+}
---
+++ checkout/src/main/java/br/roger/ecommerce/checkout/resource/CheckoutResource.java
@@ -0,0 +1,28 @@
+package br.roger.ecommerce.checkout.resource;
+
+import br.roger.ecommerce.checkout.event.CheckoutCreatedEvent;
+import br.roger.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/checkout")
+@RequiredArgsConstructor
+public class CheckoutResource {
+
+    private final CheckoutCreatedSource checkoutCreatedSource;
+
+    @PostMapping("/")
+    public ResponseEntity<Void> create() {
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode("123")
+                .build();
+//        checkoutCreatedSource.output().send(MessageBuilder.withPayload("asdasdasd").build());
+        checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
+        return ResponseEntity.ok().build();
+    }
+}
